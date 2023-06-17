2124aa133c1d971b970ba293d2d8c961d8568fe4
TeachMeTW <teachmetheways652@gmail.com>
2023-06-17 11:34:21 -0700

    test

--- LauAcademy/lauacademy/front/home.py
+++ LauAcademy/lauacademy/front/home.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+import urllib.request
 
 
-
-st.title('Mock')
+st.title('Lau Academy')
+image = Image.open('media/dab.png')
+st.image(image, caption='Kiaran - Founder | CEO | LauAcademy')
