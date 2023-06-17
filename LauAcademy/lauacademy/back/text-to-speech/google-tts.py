ecf9a48072b1fd285c49a550a08878d0a4e573ac
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-06-17 14:56:41 -0700

    google-tts classified

--- LauAcademy/lauacademy/back/text-to-speech/google-tts.py
+++ LauAcademy/lauacademy/back/text-to-speech/google-tts.py
@@ -2,36 +2,36 @@ import os
 import dotenv
 
 dotenv.load_dotenv()
-
-API_KEY = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 from google.cloud import texttospeech
 
-# Initiates the Client
-client = texttospeech.TextToSpeechClient()
-
 # Set the text input to be synthesized
-target_text = "Konstantin Victoria, the Goat of the West."
-synthesis_input = texttospeech.SynthesisInput(text=target_text)
+target_text = "This is a test. This is a test. Double 07."
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-)
+class TextToSpeech:
+    def __init__(self):
+        API_KEY = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        self.client = texttospeech.TextToSpeechClient()
+        self.voice = texttospeech.VoiceSelectionParams(
+            language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+        )
+        self.audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+        
+    
+    def synthesize_text(self, target_text):
+        synthesis_input = texttospeech.SynthesisInput(text=target_text)
+        response = self.client.synthesize_speech(
+            input=synthesis_input, voice=self.voice, audio_config=self.audio_config
+        )
+        
+        with open("output.mp3", "wb") as out:
+            # The response's audio_content is binary.
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
-
-# The response's audio_content is binary.
-with open("output.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+# Test Code
+# tts = TextToSpeech()
+# tts.synthesize_text("Test Number 2. Google AI text voices")
