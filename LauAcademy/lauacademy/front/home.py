58a2e55155eb918c8b5995d90a5bfc55c3db2ab7
Awen Li <tigerali98@yahoo.com>
2023-06-17 15:49:04 -0700

    #7 updated comments

--- LauAcademy/lauacademy/front/home.py
+++ LauAcademy/lauacademy/front/home.py
@@ -23,12 +23,10 @@ if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8")) # convert to string based IO:
     st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
+    string_data = stringio.read() # read file as string:
     st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.read_csv(uploaded_file) #to accept files
     st.write(dataframe)
     
     # print("YourPersonalAcademicBot:", response)
