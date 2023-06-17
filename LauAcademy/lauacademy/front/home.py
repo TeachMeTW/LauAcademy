78122c56cf609bb2c45ff07e68731462bee84ca7
Awen Li <tigerali98@yahoo.com>
2023-06-17 16:17:35 -0700

    #7 updated code bug changes

--- LauAcademy/lauacademy/front/home.py
+++ LauAcademy/lauacademy/front/home.py
@@ -1,22 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from io import StringIO
 from PIL import Image
 import urllib.request
 
 
 st.title('Lau Academy')
-image = Image.open('media/dab.png')
+image = Image.open('LauAcademy/lauacademy/front/media/dab.png')
 st.image(image, caption='Kiaran - Founder | CEO | LauAcademy')
 
 
 st.write("Hello! Welcome to LauAcademy chatroom, type your questions below")
 st.text_input(label="Your Question:", key="textbox1")
-st.file_uploader(label="", type=None, accept_multiple_files=False, key=None, help=None,
-    on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+# st.file_uploader(
+#     label="Upload File",
+#     type=None,
+#     accept_multiple_files=False,
+#     key=None,
+#     help=None,
+#     on_change=None,
+#     args=None,
+#     kwargs=None,
+#     disabled=False,
+#     label_visibility="visible",
+# )
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+uploaded_file = st.file_uploader("Upload Textbook (PDF)") 
+if uploaded_file is not None: # only uploads one file at a time
     bytes_data = uploaded_file.getvalue() # read file as bytes
     st.write(bytes_data)
 
@@ -26,7 +37,7 @@ if uploaded_file is not None:
     string_data = stringio.read() # read file as string:
     st.write(string_data)
 
-    dataframe = pd.read_csv(uploaded_file) #to accept files
+    dataframe = pd.read_csv(uploaded_file) # to accept files
     st.write(dataframe)
     
     # print("YourPersonalAcademicBot:", response)
