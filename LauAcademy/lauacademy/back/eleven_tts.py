cd5dd4337f328c9f93e08fad8e08fbf61640ab0a
TeachMeTW <teachmetheways652@gmail.com>
2023-06-17 20:12:16 -0700

    Working genertor

--- LauAcademy/lauacademy/back/eleven_tts.py
+++ LauAcademy/lauacademy/back/eleven_tts.py
@@ -5,6 +5,6 @@ set_api_key("19e159289a5a4b42f0f8066359b0d7ea")
 
 
 def gen(fname,text):
-    audio = generate(text=text, voice='Bella', model='eleven_monolingual_v1')
+    audio = generate(text=text, voice='Josh', model='eleven_monolingual_v1')
     save(audio, fname)
     return
--- LauAcademy/lauacademy/back/video_gen.py
+++ LauAcademy/lauacademy/back/video_gen.py
@@ -1,30 +1,19 @@
 from moviepy.editor import *
 import text_to_image
 import eleven_tts
-
-"""
-    ic_1 = ImageClip('./LauAcademy/media/test/grass.jpg').set_duration(7)
-    ic_2 = ImageClip('./LauAcademy/media/test/shower.jpg').set_duration(7)
-    ic_3 = ImageClip('./LauAcademy/media/test/woman.jpg').set_duration(7)
-    aud = AudioFileClip("./LauAcademy/media/test/imagine.mp3")
-
-    txt_clip = TextClip("Things League Players are scared of",fontsize=70,color='black')
-
-    # Say that you want it to appear 10s at the center of the screen
-    txt_clip = txt_clip.set_pos('center').set_duration(5)
-
-
-    video = concatenate([ic_1, ic_2, ic_3], method="compose")
-    test1 = CompositeVideoClip([video, txt_clip])
-    test2 = test1.set_audio(aud)
-
-    test2.write_videofile('test.mp4', fps=24)
-"""
+import nltk.data
+nltk.download('punkt')
 
 " PRE PROCESS THE TEXT HERE USING REGEX"
 
 
-
+"""
+Input:
+    Title: File Name
+    Images: List of Image Dirs
+    Audio: List of Audio Dirs
+    Text: List of Strings
+"""
 def create_slideshow(title,images, audio, text):
     
     size = range(len(images))
@@ -59,15 +48,28 @@ def create_slideshow(title,images, audio, text):
     return
 
 
-k = text_to_image.search('Map')
-im = text_to_image.get('Map',k,3)
-audio = []
-title = 'Pooper'
 
-sentence = ["In this first slide, we will learn how to work with Phoenix Channels by establishing a connection from the client to the channel running on the server.", "To accomplish this, we need to create a new socket object and pass it a path to the socket."]
+def gen_voice_img(title, sentence):
+    
+    audio = []
+    k = text_to_image.search(title)
+    img = text_to_image.get(title,k,len(sentence))
+    
+    for x in range(len(sentence)):
+        audio.append(f'{x}.mp3')
+        eleven_tts.gen(f'{x}.mp3',sentence[x])
+    return audio,img
 
-for x in range(len(sentence)):
-    audio.append(f'{x}.mp3')
-    eleven_tts.gen(f'{x}.mp3',sentence[x])
 
-create_slideshow(title=title, images=im, audio=audio, text=sentence)
+def tokenize(f):
+    
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    fp = open(f)
+    data = fp.read()
+    sentence = (tokenizer.tokenize(data))
+    return sentence
+
+sent = tokenize('test.txt')
+#print([x for x in sent])
+aud,img = gen_voice_img('Roblox', sent)
+create_slideshow('Roblox', img, aud, sent)
