c984e42703e61d3cb711736b58407eb59e7154c1
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-06-17 19:53:09 -0700

    final changes to google tts

--- LauAcademy/lauacademy/front/home.py
+++ LauAcademy/lauacademy/front/home.py
@@ -4,7 +4,28 @@ import numpy as np
 from PIL import Image
 import urllib.request
 
+import time
 
-st.title('Lau Academy')
-image = Image.open('media/dab.png')
-st.image(image, caption='Kiaran - Founder | CEO | LauAcademy')
+# st.title('Lau Academy')
+# image = Image.open('media/dab.png')
+# st.image(image, caption='Kiaran - Founder | CEO | LauAcademy')\
+
+def user_input_page():
+    st.empty()
+    st.title("LauAcademy")
+    st.write("Welcome to LauAcademy!")
+
+def different_page():
+    st.empty()
+    st.title("Different Page")
+    st.write("This is a different page.")
+
+
+pages = {
+    "Input Page": user_input_page,
+    "Different Page": different_page,
+}
+
+user_input_page()
+time.sleep(3)
+different_page()
