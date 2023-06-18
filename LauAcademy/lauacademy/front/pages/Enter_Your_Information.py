12cce651d49cfa7a00ab3190908a35f64d231820
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-06-18 10:01:57 -0700

    added a working progress bar

--- LauAcademy/lauacademy/front/pages/Enter_Your_Information.py
+++ LauAcademy/lauacademy/front/pages/Enter_Your_Information.py
@@ -3,16 +3,24 @@ from PIL import Image
 import time
 
 def callback_success():
+    progress_bar = st.progress(0, "Loading...")
     for i in range(100):
-        st.progress(i, text="Loading...")
+        progress_bar.progress(i)
         time.sleep(1/20)
     st.sidebar.success("Successfully uploaded your textbook!\nCheck the \"See The Result\" Tab.")
-    
+
+def onclick_confirm():
+    print('clicked confirm!')
+    st.sidebar.success("Successfully confirmed your user data.")
+    pass
 
 image = Image.open('LauAcademy/lauacademy/media/enter-your-information-high-resolution-color-logo.png')
 st.image(image)
 st.subheader("This will help shape your lecture plans.")
-st.text_input(label="What is your educational background?", key="t1")
-st.text_input(label="What is your desired career?", key="t2")
-st.text_input(label="What is the subject of this class?", key="t3")
+name = st.text_input(label="What is your name?", key="t0")
+educational_background = st.text_input(label="What is your educational background?", key="t1")
+desired_career = st.text_input(label="What is your desired career?", key="t2")
+class_subject = st.text_input(label="What is the subject of this class?", key="t3")
 uploaded_file = st.file_uploader("Attach your Textbook's PDF here:", on_change=callback_success) 
+
+confirmation_button = st.button(label="Confirm", on_click=onclick_confirm)
