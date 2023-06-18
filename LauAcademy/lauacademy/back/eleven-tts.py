63c5d30c7150015f904fb7915b8c026a9ebf6387
TeachMeTW <teachmetheways652@gmail.com>
2023-06-17 20:18:44 -0700

    removed dups

--- LauAcademy/lauacademy/back/eleven-tts.py
+++
@@ -1,8 +0,0 @@
-from elevenlabs import clone, generate, play, set_api_key
-from elevenlabs.api import History
-
-set_api_key("7beb515e2721eef8f3ccf7ae1d325f8b")
-
-audio = generate(text="Robin, the Rizzler of the East. His superpower; trauma from his past relationships. He is a man of Fortune and Free Will", voice='Bella', model='eleven_monolingual_v1')
-
-play(audio)
