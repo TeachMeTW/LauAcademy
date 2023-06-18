54cb739c57d13afb7f425b1757934dd16134350d
Awen Li <tigerali98@yahoo.com>
2023-06-18 11:06:59 -0700

    #7 added video player and caption box below

--- LauAcademy/lauacademy/front/pages/See_The_Result.py
+++ LauAcademy/lauacademy/front/pages/See_The_Result.py
@@ -3,3 +3,9 @@ from PIL import Image
 
 image = Image.open('LauAcademy/lauacademy/media/see-the-result-high-resolution-color-logo.png')
 st.image(image)
+
+video_file = open('LauAcademy\lauacademy\media\TestSpidermanMV.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+
+st.write('*Hello* :sunglasses:')
